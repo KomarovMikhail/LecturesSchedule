a35e84f60aeebb53651d55fee518acd8c7d524aa
Komarov Mikhail <mihanya009@mail.ru>
2018-07-14 16:42:37 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+token = '588048354:AAFgl8K6V4JSoc9Cs3v4PlgNT2zfNHg0XcU'
+
+URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQaIuizRlU2hODjbpXeydGqq67f5-cU1G7-7xIgMs7m9bpiAMRsO6pt27_gFKozauiudddO4pn_3HA8/pub?output=xlsx'
+XML_PATH = 'files/lectures.xml'
+XLSX_PATH = 'files/lectures.xlsx'
---
+++ main2.py
@@ -0,0 +1,84 @@
+import urllib.request
+import config
+import xlrd
+from lxml import etree
+from datetime import datetime
+
+
+def download_spreadsheet(from_path, to_path):
+    with urllib.request.urlopen(from_path) as url:
+        spreadsheet = url.read()
+    file = open(to_path, "wb")
+    file.write(spreadsheet)
+    file.close()
+
+
+def convert_to_xml(from_path, to_path):
+    root = etree.Element('lectures')
+    wb = xlrd.open_workbook(from_path)
+    sh = wb.sheet_by_index(0)
+
+    for row in range(1, sh.nrows):
+        val = sh.row_values(row)
+        element = etree.SubElement(root, 'lecture')
+        element.set('id', str(int(val[0])))
+        element.set('date', str(val[1]))
+        element.set('start', str(val[2]))
+        element.set('end', str(val[3]))
+        element.set('name', str(val[4]))
+        element.set('lecturer', str(val[5]))
+
+    with open(to_path, 'wb') as out:
+        out.write(etree.tostring(root, pretty_print=True, encoding='UTF-8'))
+
+
+def parse_xml(path):
+    with open(path) as file:
+        xml = file.read()
+
+    result = []
+
+    root = etree.fromstring(xml)
+    for child in root.getchildren():
+        result.append(child.attrib)
+    return result
+
+
+def is_upcoming(lecture):
+    date = lecture['date'].split('.')
+    day = int(date[0])
+    month = int(date[1])
+
+    lecture_time = lecture['start'].split(':')
+    hour = int(lecture_time[0])
+    minute = int(lecture_time[1])
+
+    result_time = datetime(datetime.now().year, month, day, hour, minute)
+
+    return result_time > datetime.now()
+
+
+def sort_key(lecture):
+    date = lecture['date'].split('.')
+    day = int(date[0])
+    month = int(date[1])
+
+    lecture_time = lecture['start'].split(':')
+    hour = int(lecture_time[0])
+    minute = int(lecture_time[1])
+
+    return datetime(datetime.now().year, month, day, hour, minute)
+
+
+def get_nearest(xml_file):
+    lectures = parse_xml(xml_file)
+
+    upcoming = [lecture for lecture in lectures if is_upcoming(lecture)]
+    upcoming = sorted(upcoming, key=sort_key)
+    print(upcoming)
+    return upcoming[:3]
+
+
+download_spreadsheet(config.URL, config.XLSX_PATH)
+convert_to_xml(config.XLSX_PATH, config.XML_PATH)
+print(get_nearest(config.XML_PATH))
