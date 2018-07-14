199af0ef95c64ca07cbf3f66a198f3529820d4b4
Komarov Mikhail <mihanya009@mail.ru>
2018-07-14 21:29:25 +0300

    demo

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, 'Bot: ' + message.text)
 
 
 if __name__ == '__main__':
