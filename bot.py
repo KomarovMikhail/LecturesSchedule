da8482844861f9cca633fc33fb37c26028e42592
Komarov Mikhail <mihanya009@mail.ru>
2018-07-15 16:20:02 +0300

    webhook

--- bot.py
+++ bot.py
@@ -4,34 +4,25 @@ import telebot
 from sslib import get_nearest, download_spreadsheet, convert_to_xml
 import os
 from flask import Flask, request
-import flask
 import logging
 
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
 bot = telebot.TeleBot(config.TOKEN)
 
-server = Flask(__name__)
-
-
-# Empty webserver index, return nothing, just http 200
-@server.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
-
-
-# Process webhook calls
-@server.route(config.WEBHOOK_URL_PATH, methods=['POST'])
-def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data()
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_messages([update.message])
-        return ''
-    else:
-        flask.abort(403)
+# @server.route('/', methods=['GET', 'HEAD'])
+# def index():
+#     return ''
+#
+#
+# @server.route(config.WEBHOOK_URL_PATH, methods=['POST'])
+# def webhook():
+#     if flask.request.headers.get('content-type') == 'application/json':
+#         json_string = flask.request.get_data()
+#         update = telebot.types.Update.de_json(json_string)
+#         bot.process_new_messages([update.message])
+#         return ''
+#     else:
+#         flask.abort(403)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -62,38 +53,34 @@ def unknown_messages(message):
     bot.send_message(message.chat.id, "Sorry, I don't understand you, i'm just a machine :-(")
 
 
-# @server.route("/" + config.TOKEN, methods=['POST'])
-# def get_message():
-#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-#     return "!", 200
-#
-#
-# @server.route("/")
-# def webhook():
-#     # bot.remove_webhook()
-#     # bot.set_webhook(url="https://heroku/" + config.TOKEN + "/")
-#     bot.remove_webhook()
-#
-#     # Ставим заново вебхук
-#     bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-#                     certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
-#     return "!", 200
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+server = Flask(__name__)
+
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://voyage-livre-89482.herokuapp.com/bot")
+    return "?", 200
 
 
 if __name__ == '__main__':
-    bot.remove_webhook()
-
-    # Set webhook
-    bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
-                    certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
-
-    # Start flask server
-    server.run(host=config.WEBHOOK_LISTEN,
-               port=config.WEBHOOK_PORT,
-               ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
-               debug=True)
-    # server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-    # server = Flask(__name__)
+    # bot.remove_webhook()
+    # bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
+    #                 certificate=open(config.WEBHOOK_SSL_CERT, 'r'))
+    # server.run(host=config.WEBHOOK_LISTEN,
+    #            port=config.WEBHOOK_PORT,
+    #            ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
+    #            debug=True)
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 #     while True:
 #         try:
 #             bot.polling(none_stop=True)
