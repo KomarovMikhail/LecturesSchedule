4d75948bde50de6f15ee3d6aa2875cf2d5e66aa1
Komarov Mikhail <mihanya009@mail.ru>
2018-07-15 15:30:41 +0300

    webhook

--- config.py
+++ config.py
@@ -4,3 +4,13 @@ TOKEN = '588048354:AAFgl8K6V4JSoc9Cs3v4PlgNT2zfNHg0XcU'
 URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQaIuizRlU2hODjbpXeydGqq67f5-cU1G7-7xIgMs7m9bpiAMRsO6pt27_gFKozauiudddO4pn_3HA8/pub?output=xlsx'
 XML_PATH = 'files/lectures.xml'
 XLSX_PATH = 'files/lectures.xlsx'
+
+WEBHOOK_HOST = 'voyage-livre-89482.herokuapp.com'
+WEBHOOK_PORT = 443
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (TOKEN)
