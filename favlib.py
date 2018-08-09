b137b3c47e258758e98fd92cdfec2ee83aa8b591
Komarov Mikhail <mihanya009@mail.ru>
2018-08-09 22:00:05 +0300

    Add delete query

--- favlib.py
+++ favlib.py
@@ -38,8 +38,11 @@ def remove_from_favorite(cid, lid):
         old_ids = data[0][0].split(',')
         try:
             old_ids.remove(lid)
-            new_ids = ','.join(old_ids)
-            cursor.execute(UPDATE.format(cid, new_ids))
+            if len(old_ids) == 0:
+                cursor.execute(DELETE.format(cid))
+            else:
+                new_ids = ','.join(old_ids)
+                cursor.execute(UPDATE.format(cid, new_ids))
         except ValueError:
             pass
     conn.commit()
